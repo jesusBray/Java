ae7fa7ace2f0c7b44f03e1667147c9607141997a
JesusBryanPaye <jesus.brayan.paye@gmail.com>
2018-06-01 21:50:27 -0400

    Una ventana emergente

---
+++ JFrames/Main.java
@@ -0,0 +1,9 @@
+package JFrames;
+import javax.swing.*;
+public class Main{
+    public static void main(String[] arg){
+        Marco1 marco = new Marco1();
+        marco.setVisible(true);
+        marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
