d902c78a2670e8012909f5be29f291a8091b6517
JesusBryanPaye <jesus.brayan.paye@gmail.com>
2018-06-02 21:53:05 -0400

    Adding example exception

---
+++ Exsepciones/Main.java
@@ -0,0 +1,9 @@
+package Exsepciones;
+public class Main{
+    public static void main(String[] arg){
+        Excepcion1 ex = new Excepcion1(2,0);
+        System.out.println(ex.getNum1());
+
+
+    }
+}
