9dbf8cd4aa9c7cf07a2865109c4052a72fd89e98
JesusBryanPaye <jesus.brayan.paye@gmail.com>
2018-06-02 21:55:50 -0400

    fixed main fail

--- Exsepciones/Main.java
+++ Exsepciones/Main.java
@@ -1,9 +1,8 @@
 package Exsepciones;
+
 public class Main{
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
         Excepcion1 ex = new Excepcion1(2,0);
         System.out.println(ex.getNum1());
-
-
     }
 }
