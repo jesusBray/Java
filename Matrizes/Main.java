097273005f4a656733a46ce9d740f425cda99500
jesusBray <jesus.brayan.paye@gmail.com>
2018-06-07 22:53:17 -0400

    example matrices

---
+++ Matrizes/Main.java
@@ -0,0 +1,30 @@
+package Matrizes;
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        
+        Scanner entrada = new Scanner(System.in);
+        int[][] mz;
+
+        System.out.println("Digite numero de columnas de la matriz");
+        int filas = entrada.nextInt();
+        System.out.println("Digite numero de columnas de la matriz");
+        int columnas = entrada.nextInt();
+        mz = new int[filas][columnas];
+        
+        System.out.println("Digite numero de elementos de la matriz");
+        for(int i=0;i<filas;i++){
+            for(int j=0;j<columnas;j++){
+                mz[i][j] = entrada.nextInt();
+            }
+        }
+
+        for(int i=0;i<filas;i++){
+            for(int j=0;j<columnas;j++){
+                System.out.print(mz[i][j]);
+            }
+            System.out.print("");
+        }
+        System.out.println("reposito java");
+    }
+}
---
+++ Matrizes/Matriz.java
@@ -0,0 +1,13 @@
+package Matrizes;
+public class Matriz{
+    private int fila,columna;
+
+    public Matriz(int fila,int columna){
+        this.fila=fila;
+        this.columna=columna;
+    }
+
+    public String MostrarDatos(){
+        return "Datos: ["+fila+"] ["+columna+"]";
+    }
+}
