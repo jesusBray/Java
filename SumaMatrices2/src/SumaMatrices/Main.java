ec584e4fc727f5787aba596900dba9381c28f033
jesusBray <jesusBray>
2018-08-12 19:37:00 -0400

    exale completed

--- SumaMatrices2/src/SumaMatrices/Main.java
+++ SumaMatrices2/src/SumaMatrices/Main.java
@@ -2,13 +2,7 @@ package SumaMatrices;
 
 public class Main{
     public static void main(String[] args) {
-        new Suma().verSuma(Matriz1(), Matriz2());
+        Suma suma = new Suma();
+        suma.verSuma(suma.Matriz(), suma.Matriz());
     }
-    public static int[][] Matriz1(){
-        return new int[0][0];
-    }
-    
-    public static int[][] Matriz2(){
-        return new int[0][0];
-    } 
 }
