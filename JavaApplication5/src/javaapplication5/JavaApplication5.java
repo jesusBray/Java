d90c892bd0f6279ff617dffcd3f250b23b7911be
jesusBray <jesus.brayan.paye@gmail.com>
2018-08-15 18:59:11 -0400

    exaple palindrome execute

--- JavaApplication5/src/javaapplication5/JavaApplication5.java
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -4,9 +4,10 @@ package javaapplication5;
 public class JavaApplication5 {
 
     public static void main(String[] args) {
-        //new ShowPalindrome(new Palindrome("ala")).ShowPalindrome();
-        new ShowPalindrome().ShowPalindrome("raro");
+        new ShowPalindrome(new Palindrome("ala")).ShowPalindrome();
         
+        
+        new ShowPalindrome().ShowPalindrome("ala");
     }
     
 }
