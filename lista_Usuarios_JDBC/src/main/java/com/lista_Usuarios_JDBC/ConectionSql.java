a469b5ce539eebd9631d4bea862ce6b31ec92553
jesusBray <jesus.brayan.paye@gmail.com>
2018-08-21 23:22:21 -0400

    exaple usuign data base

--- lista_Usuarios_JDBC/src/main/java/com/lista_Usuarios_JDBC/ConectionSql.java
+++ lista_Usuarios_JDBC/src/main/java/com/lista_Usuarios_JDBC/ConectionSql.java
@@ -4,38 +4,29 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConectionSql {
-    private String user,password;
+    private String path,user,password;
     private Connection myConnection = null;
     
-    public ConectionSql(String user,String password) {
+    public ConectionSql(String path, String user,String password) {
+        this.path = path;
         this.user=user;
         this.password = password;
-    }
-    
-    public boolean isConnected() {
-        try{
-            System.out.println("verifcando datos ");
-            if (Connect().isClosed()) {
-                System.out.println("La base de datos esta apagada");
-                return false;
-            }
-        }catch (Exception e) {
-            System.out.println("No se pudo estableder coneccion " + e.getMessage());
-            e.getStackTrace();
-        }
-        return true;
     }
     
     public Connection Connect() {
         try {
             System.out.println("coneccion establesida ");
-            return myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/USUARIO",this.user,this.password);
+            return myConnection = DriverManager.getConnection(this.path,this.user,this.password);
         }catch (Exception e) {
-            System.out.println("Error no coneccion no establecida");
+            System.out.print(e.getMessage());
         }
         return myConnection;
     }
-
-
-    
+    public void ExitConnection() {
+        try {
+            myConnection.close();
+        } catch (Exception e) {
+            System.out.print("erroe en el cierre de conexion"+e.getMessage());
+        }
+    }
 }
