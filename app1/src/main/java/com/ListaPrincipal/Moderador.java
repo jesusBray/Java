0b1bed4a9f7413a6873746a0dd6c1079d11133ad
jesusBray <jesus.brayan.paye@gmail.com>
2018-08-29 20:56:11 -0400

    update class

--- app1/src/main/java/com/ListaPrincipal/Moderador.java
+++ app1/src/main/java/com/ListaPrincipal/Moderador.java
@@ -6,6 +6,7 @@ import com.ListaPrincipal.Mysql.UsuarioMysql;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 
 public class Moderador {
@@ -13,10 +14,35 @@ public class Moderador {
     UsuarioMysql user = null;
     Connection conn = null;
     
-    public void Path(String path){   
+    public void DBConnect(){
+        //String userDir = System.getProperty("user.dir");
+        //String propPath = MessageFormat.format("{0}/MyDBConfig.properties", userDir);
+        config = new DBConfig("./Config/dbconfig.properties");
+        String server = config.getServer();
+        String port = config.getPort();
+        String db = config.getBaseDato();
+        String user = config.getUser();
+        String pass = config.getPassword();
+        try {
+            String stringDriver = MessageFormat.format("jdbc:mysql://{0}:{1}/{2}?autoReconnect=true&useSSL=false", server, port, db);
+            System.out.println(stringDriver);
+            //conn = DriverManager.getConnection("jdbc:mysql://"+server+":"+port+"/"+db+"?autoReconnect=true&useSSL=false",user,pass);
+            conn = DriverManager.getConnection(stringDriver, user, pass);
+        } catch (SQLException ex) {
+            System.out.println("error en :"+ex.getLocalizedMessage());
+        }
+    }
+    
+    public void Path(String path) {
         try {
             config = new DBConfig(path);
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/users?autoReconnect=true&useSSL=false",config.getUser(),config.getPassword());
+            String server = config.getServer();
+            String port = config.getPort();
+            String db = config.getBaseDato();
+            String user = config.getUser().toString();
+            String pass = config.getPassword().toString();
+            //conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/users?autoReconnect=true&useSSL=false",config.getUser(),config.getPassword());
+            conn = DriverManager.getConnection("jdbc:mysql://"+server+":"+port+"/"+db+"?autoReconnect=true&useSSL=false",user,pass);
         } catch (SQLException ex) {
             System.out.println("error en :"+ex.getLocalizedMessage());
         }
