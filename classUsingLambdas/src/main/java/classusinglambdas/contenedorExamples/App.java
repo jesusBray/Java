ba9fe3cac3197d581e993182462006042e636195
jesusBray <jesus.brayan.paye@gmail.com>
2018-09-10 18:12:37 -0400

    class ugin lambdas

---
+++ classUsingLambdas/src/main/java/classusinglambdas/contenedorExamples/App.java
@@ -0,0 +1,16 @@
+package classusinglambdas.contenedorExamples;
+
+import classusinglambdas.contenedorExamples.example1.example1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        example1 e = new example1();
+        e.ordenarLambda();
+    }
+}
---
+++ classUsingLambdas/src/main/java/classusinglambdas/contenedorExamples/example1/example1.java
@@ -0,0 +1,23 @@
+package classusinglambdas.contenedorExamples.example1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class example1 {
+    
+    public void ordenarLambda(){
+        ArrayList<String> lista = new ArrayList<String>();
+        lista.add("jorge");
+        lista.add("jose");
+        lista.add("felipe");
+        lista.add("rodrigo");
+        
+        Collections.sort(lista, (String a1,String a2) -> a1.compareTo(a2));
+        for (String string : lista) {
+            System.out.println(string);
+        }
+        
+    }
+    
+    
+}
