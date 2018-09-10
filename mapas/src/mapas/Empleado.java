740969e82093e9c4624fd8741f7bc5c61fd2d135
jesusBray <jesus.brayan.paye@gmail.com>
2018-09-09 20:19:30 -0400

    examples update

---
+++ mapas/src/mapas/Empleado.java
@@ -0,0 +1,17 @@
+package mapas;
+
+public class Empleado {
+    
+    private String nombre,apellido;
+    private int edad;
+    public Empleado(String nombre,String apellido, int edad) {
+        this.nombre=nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+    
+    @Override
+    public String toString(){
+        return "Nombre: "+nombre+" Apellido: "+apellido+" Edad: "+edad;
+    }
+}
