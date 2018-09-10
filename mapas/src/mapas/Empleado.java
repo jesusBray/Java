2c904894af28dd3d189cd75e807017740467a3ba
jesusBray <jesus.brayan.paye@gmail.com>
2018-09-10 18:13:31 -0400

    class using maps

--- mapas/src/mapas/Empleado.java
+++ mapas/src/mapas/Empleado.java
@@ -4,6 +4,7 @@ public class Empleado {
     
     private String nombre,apellido;
     private int edad;
+    
     public Empleado(String nombre,String apellido, int edad) {
         this.nombre=nombre;
         this.apellido = apellido;
---
+++ mapas/src/mapas/Listar.java
@@ -0,0 +1,15 @@
+
+package mapas;
+
+import java.util.HashMap;
+
+public class Listar {
+    
+    Empleado e;
+    
+    public void listar(HashMap<String , Empleado> listMap){
+        
+        
+    }
+            
+}
