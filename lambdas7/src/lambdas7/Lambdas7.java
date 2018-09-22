7d0239830e668be488d69dffe8ff4cca3068394f
jesusBray <jesus.brayan.paye@gmail.com>
2018-09-21 22:11:11 -0400

    update class

--- lambdas7/src/lambdas7/Lambdas7.java
+++ lambdas7/src/lambdas7/Lambdas7.java
@@ -20,29 +20,57 @@ public class Lambdas7 implements iOperacion1,iOperacion2{
         metodoDefault4();
     }
     
+    public void metodoCorriente(){
+//-----------------metodo anonimo-------------------------------
+        Arrays.sort(nombres, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+//-----------------------expreion lambdas----------------------        
+        Arrays.sort(nombres, (a,b)-> a.compareToIgnoreCase(b));
+        System.out.println(Arrays.toString(nombres));
+//        ----------------referencia de metodos------------------
+        Arrays.sort(nombres,String::compareTo);
+        System.out.println(Arrays.toString(nombres));
+        
+    }
+    
     public static void metodoEstatico(){
         System.out.println("hola desde un metodo estatico! ");
     }
     
-    public void metodoCorriente(){
-        Arrays.sort(nombres, (a,b)-> a.compareTo(b));
-        System.out.println(Arrays.toString(nombres));
+    @Override
+    public void metodoDefault3(){
+        iOperacion5 op5 = (nota) -> {
+            Lambdas7.metodoEstatico();
+            return nota+"......";
+        };
+        System.out.println(op5.metodoFunctionalInterface("esta es una nota aderida al metodo metodoDefault3"));
+    }
+    
+    public void metodoCorriente1(){
+        System.out.println("hola todos desde el metodoCorriente 1!!!");
 
     }
     
-    @Override
-    public void metodoDefault3(){
-        iOperacion3 op3= () -> {
-            Lambdas7.metodoEstatico();
-        };
-        op3.metodoFunctionalInterface();
+    public void metodoConstructor(){
+        
+        iOperacion3 op5 = (int id, String nombre, String gerarq) -> new Persona(id, nombre, gerarq);
+        Persona per = op5.crearPersona(1, "jesus", "vice precidente junior");
+        System.out.println(per.toString());
+        
+//----------------------------tambien referencia de metodos*-----------------------
+
+        iOperacion3 op3 = Persona::new;
+        Persona per2 = op3.crearPersona(2, "brayan", "precidente junior");
+        System.out.println(per2.toString());
     }
     
     public static void main(String[] args) {
         Lambdas7 lm = new Lambdas7();
-        System.out.println(Arrays.toString(lm.nombres));
-        System.out.println("Despues! \n");
-        lm.metodoCorriente();
+        lm.metodoConstructor();
     }
 }
   
