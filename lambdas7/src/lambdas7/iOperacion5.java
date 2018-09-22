e8f93913cf271c2af164ac9771697f611a591e67
jesusBray <jesus.brayan.paye@gmail.com>
2018-09-21 22:14:38 -0400

    update class

---
+++ lambdas7/src/lambdas7/iOperacion5.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lambdas7;
+
+/**
+ *
+ * @author jesus
+ */
+public interface iOperacion5 {
+    String metodoFunctionalInterface(String nota);
+}
