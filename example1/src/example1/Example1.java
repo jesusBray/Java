68286306037445a04513c7195f1eea167e8edaea
jesusBray <jesus.brayan.paye@gmail.com>
2018-11-06 07:21:20 -0400

    example class java

---
+++ example1/src/example1/Example1.java
@@ -0,0 +1,17 @@
+
+package example1;
+
+public class Example1 {
+
+    private static int i=3;
+    
+    public static void main(String[] args) {
+        
+        for (new Example1().i = 10; new Example1().i < 100; new Example1().i ++) {
+            System.out.println(i);
+        }
+        
+        
+    }
+    
+}
