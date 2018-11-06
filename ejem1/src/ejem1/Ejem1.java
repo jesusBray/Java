92636e9518342e76f9b02fe484f32c8c4b1e46e0
jesusBray <jesus.brayan.paye@gmail.com>
2018-11-06 07:20:30 -0400

    class example

---
+++ ejem1/src/ejem1/Ejem1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejem1;
+
+import ejem2.clase1;
+
+/**
+ *
+ * @author jesusbook
+ */
+public class Ejem1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        
+    }
+    
+}
---
+++ ejem1/src/ejem2/clase1.java
@@ -0,0 +1,31 @@
+
+package ejem2;
+
+public class clase1 {
+    
+    private String nombre,apellido;
+
+    public clase1(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    
+     String getNombre() {
+        return nombre;
+    }
+
+     void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+     String getApellido() {
+        return apellido;
+    }
+
+     void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    
+}
