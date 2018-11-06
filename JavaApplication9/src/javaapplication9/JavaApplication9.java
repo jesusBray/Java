fdbc3f550563eaa891caad0ec96c0420daf5a9c2
jesusBray <jesus.brayan.paye@gmail.com>
2018-11-06 07:23:51 -0400

    example class

---
+++ JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -0,0 +1,17 @@
+
+package javaapplication9;
+
+import java.util.ArrayList;
+
+public class JavaApplication9 <T> extends Persona{
+    
+    public ArrayList<T> ordenarAlfabeticamente(ArrayList<T > listado){
+        return null;
+    }
+    
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
