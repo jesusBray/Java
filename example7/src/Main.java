2fb2a663ae8e9671052ae25fdf7f27f6b8957f29
Jesus Br P <jesus.brayan.paye@gmail.com>
2019-05-17 07:18:24 -0400

    generate example using java 8 'Lambdas'

---
+++ example7/src/Main.java
@@ -0,0 +1,14 @@
+import otherPackage.Estudiante;
+
+public class Main {
+
+    Estudiante est;
+    public static void method1(Estudiante s){
+        System.out.println("aaaa");
+    }
+            
+    public static void main(String[] args) {
+        
+    }
+    
+}
